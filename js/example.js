8d55914f75bf1070048da26d7b72013035de20d6
boris.momtchev <boris.momtchev@gmail.com>
2015-03-14 16:05:04 -0500

    Doing ajax to get data frome server

--- js/example.js
+++ js/example.js
@@ -6,7 +6,20 @@ angular.module('ui.bootstrap.demo').controller('TypeaheadCtrl', function($scope,
 
   // suggestions api
   $scope.currentSuggestion = [];
-  $scope.suggestions = ['USA','UK','BG','BE','GR']
+
+  // $scope.suggestions = ['USA','UK','BG','BE','GR']
+  $scope.getSuggestions = function(val) {
+    return $http.get('http://23.30.242.21:82/SearchApi/GetHints/', {
+      params: {
+        q: val,
+        searchAdapter: 'FileAdapter'
+      }
+    }).then(function(response){
+        // console.log(response.data);
+        return response.data;
+    });
+  };
+
 
   // user has selected on typeahead
   $scope.onSelect = function($item) {
@@ -21,11 +34,12 @@ angular.module('ui.bootstrap.demo').controller('TypeaheadCtrl', function($scope,
   // search
   $scope.search = function($item) {
     $scope.userInput  = document.getElementById("txt_search").value;
-    $scope.countriesResult = $scope.countries;
+    $scope.countriesResult = $scope.getFiles($scope.userInput);
   };
 
   // countries result api
-  $scope.countriesResult = [];
+  // $scope.countriesResult = [];
+  /*
   $scope.countries = [
     {
       Symbol: 'USA',
@@ -48,5 +62,23 @@ angular.module('ui.bootstrap.demo').controller('TypeaheadCtrl', function($scope,
       Name: 'Greece'
     }
   ];
+  */
+
+  // http://www.nodewiz.biz/angular-js-final-callback-after-ng-repeat/
+  $scope.getFiles = function(val) {
+    return $http.get('http://23.30.242.21:82/SearchApi/GetResults/', {
+      params: {
+        q: val,
+        filter: '',
+        searchAdapter: 'FileAdapter',
+        searchAttr: true,
+        searchContent: true,
+        skip: 0
+      }
+    }).then(function(response){
+        // console.log(response.data.data.items);
+        return response.data.data.items;
+    });
+  };
 
 });
